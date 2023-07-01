9ddb034e9f959a8ab551db6240f8d32f211316e2
Uxair Abbas <uzaaaaaair@gmail.com>
2023-07-02 02:35:59 +0500

    completed writable stream

--- advanced_streams/02_04_writeable_streams/start/index.js
+++ advanced_streams/02_04_writeable_streams/start/index.js
@@ -1,8 +1,9 @@
-const { createReadStream } = require('fs');
+const { createReadStream, createWriteStream } = require('fs');
 const readStream = createReadStream('../../powder-day.mp4');
+const writeStream = createWriteStream('./copy.mp4')
 
 readStream.on('data', (chunk) => {
-    console.log('size: ', chunk.length);
+    writeStream.write(chunk);
 });
 
 readStream.on('error', (error) => {
@@ -10,5 +11,9 @@ readStream.on('error', (error) => {
 });
 
 readStream.on('end', () => {
-    console.log('done!');
+    writeStream.end();
 });
+
+writeStream.on('close', () => {
+    process.stdout.write('file copied\n');
+})
